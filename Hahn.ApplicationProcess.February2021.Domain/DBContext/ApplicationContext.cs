649f77f0b801e6fc85cf266791b1e4146499f570
Ekene Duru <ekene.duru@jaffatech.com>
2021-03-26 13:45:44 +0100

    Add project files.

---
+++ Hahn.ApplicationProcess.February2021.Domain/DBContext/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Hahn.ApplicationProcess.February2021.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicationProcess.February2021.Domain.DBContext
+{
+   public class ApplicationContext: IdentityDbContext
+    {
+        public DbSet<Asset> Asset { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ Hahn.ApplicationProcess.February2021.Domain/Validators/AssetValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Hahn.ApplicationProcess.February2021.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicationProcess.February2021.Domain.Validators
+{
+    public class AssetValidator : AbstractValidator<Asset>
+    {
+        public AssetValidator()
+        {
+            RuleFor(x => x.AssetName).MaximumLength(5);
+            RuleFor(x => x.Department).IsInEnum();
+            RuleFor(x => x.EMailAdressOfDepartment).EmailAddress();
+            RuleFor(x => x.Broken).NotNull();
+            RuleFor(x => x.PurchaseDate).GreaterThanOrEqualTo(DateTime.UtcNow.AddYears(-1));
+        }
+    
+    }
+}
